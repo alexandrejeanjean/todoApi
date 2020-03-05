0baad2785e72531f037e9bbef253971f1ac28ed6
Alexandre Jeanjean <alexandre@dotmind.io>
2020-03-05 17:22:10 +0100

    [Route, controller] Added last routes to complete the tutorial

--- server/controllers/todositems.js
+++ server/controllers/todositems.js
@@ -14,5 +14,43 @@ module.exports = {
     return TodoItem.findAll()
       .then(todoItems => res.status(200).send(todoItems))
       .catch(error => res.status(400).send(error));
+  },
+
+  update(req, res) {
+    return TodoItem.findOne({
+      where: {
+        id: req.params.todoItemId,
+        todoId: req.params.todoId
+      }
+    })
+      .then(todoItem => {
+        if (!todoItem) {
+          return res.status(404).send({ message: "Item not found" });
+        }
+        return todoItem
+          .update(req.body, { fields: Object.keys(req.body) })
+          .then(updatedTodoItem => res.status(200).send(updatedTodoItem))
+          .catch(error => res.status(400).send(error));
+      })
+      .catch(error => res.status(400).send(error));
+  },
+
+  destroy(req, res) {
+    return TodoItem.findOne({
+      where: {
+        id: req.params.todoItemId,
+        todoId: req.params.todoId
+      }
+    })
+      .then(todoItem => {
+        if (!todoItem) {
+          return res.status(404).send({ message: "Item not found" });
+        }
+        return todoItem
+          .destroy()
+          .then(() => res.status(200).send({ message: "Destroyer wins !" }))
+          .catch(error => res.status(400).send(error));
+      })
+      .catch(error => res.status(400).send(error));
   }
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -15,5 +15,16 @@ module.exports = app => {
   app.delete("/api/todos/:todoId", todosController.destroy);
 
   app.post("/api/todos/:todoId/items", todoItemsController.create);
-  app.get("/api/todos/:id/items", todoItemsController.list);
+  app.get("/api/todos/:todoId/items", todoItemsController.list);
+  app.put("/api/todos/:todoId/items/:todoItemId", todoItemsController.update);
+  app.delete(
+    "/api/todos/:todoId/items/:todoItemId",
+    todoItemsController.destroy
+  );
+
+  app.all("/api/todos/:todoId/items", (req, res) =>
+    res.status(405).send({
+      message: "Your Not Allowed to do that kid!"
+    })
+  );
 };
